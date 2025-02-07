dfffd8ec52bfc2043b2561033619c05e5e0a43d7
Pedro César <pedrocbdm26@gmail.com>
2025-02-07 19:53:35 -0300

    Calculadora2000 V3! Agora a Calculadora2000 consegue realizar operações envolvendo raízes! Leves aprimoramentos na personalidade dela também.

--- calculadora2000.py
+++ calculadora2000.py
@@ -1,17 +1,21 @@
 def entidade(a):
     if a == "me ajude calculadora2000":
-        print("com c maiusculo, vou deixar isso passar dessa vez")
+        while a != 'me ajude Calculadora2000': #nota de aprendizado: no uso de 'while', não é correto, em maior parte dos casos, adicionar um 'return' na sua indentação, nesse caso, logo abaixo da input, pois isso vai encerrar a cadeia do 'while'. Então, a posição correta do 'return' é na mesma indentação do 'while', assim, o return só irá fornecer um valor após a condição do 'while' ser cumprida. 
+            a = input("Como ousa me desrespeitar??? Calculadora2000, com C maiúsculo. Tente pedir ajuda de novo, direitinho: ")
         return a 
-    elif a == 'me ajude Calculadora2000':
-        return a
+    elif a == 'me ajude Calculadora2000': #input correta
+        print('Certo, irei conceder meu conhecimento a você')
+        return a 
     elif a == 'me ajude Calculadora 2000':
-        print('Calculadora2000, tudo junto, voce não entende?')
+        while a != 'me ajude Calculadora2000':
+           a = input('Calculadora2000, tudo junto, voce não entende? Peça ajuda de novo: ')
         return a
     elif a == 'me ajude calculadora 2000':
-        print('C maiusculo, Calculadora2000, tudo junto, voce está testando minha paciencia')
+        while a != 'me ajude Calculadora2000':
+           a = input('C maiúsculo, Calculadora2000, tudo junto, voce está testando minha paciência. Vamos lá, tente pedir ajuda mais uma vez: ')
         return a
         
-clamar = input("se ajoelhe e clame pela a ajuda Calculadora2000 *me ajude Calculadora2000*: ") #input que define a variável que vai ser usada na fórmula 
+clamar = input("se ajoelhe e clame pela a ajuda da Calculadora2000 *me ajude Calculadora2000*: ") #input que define a variável que vai ser usada na fórmula 
 
 print(entidade(clamar)) #esse comando vai printar o resultado da formula "entidade" de acordo com a input realizada no comando acima
         
@@ -24,16 +28,18 @@ def calculadora(n1,op,n2):
         return n1*n2
     elif op == 'dividir':
         return n1/n2
-    elif op == 'potencia':
+    elif op == 'potencia': 
         x = n1
         while n2 > 1:
             x = (x * n1)
             n2 = n2 - 1
         return x
+    elif op == 'raiz':
+        return n1 ** (1/n2)
         
 numero1 = int(input("digite um valor: "))
-operacao = input("o que deseja fazer? Somar, dividir, multiplicar, subtrair, potencia?: ").lower() #o ".lower()" é responsável por transcrever a input do usuário para letras minúsculas
+operacao = input("o que deseja fazer? Somar, dividir, multiplicar, subtrair, potencia, raiz?: ").lower() #o ".lower()" é responsável por transcrever a input do usuário para letras minúsculas
 numero2 = int(input("digite o segundo valor: "))
 
 print(calculadora(numero1,operacao,numero2))
-print('Não precisa agradecer à Grande Calculadora2000, ela não tem tempo para ouvir isso')
+print('Não precisa agradecer a Grande Calculadora2000, ela não tem tempo para ouvir isso')
